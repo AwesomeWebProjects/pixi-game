81a1b9b3f7dad7aba30f464ea8cc8f127f562cb3
Daniel Barion <danielbarionn@gmail.com>
2024-04-01 10:28:25 -0300

    chore: update project start script

---
+++ prebuild.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const { rimraf } = require('rimraf')
+
+const args = process.argv.slice(2)
+const parameters = args.reduce((acc, arg) => {
+  const [key, value] = arg.split('=')
+  acc[key.replace('--', '')] = value
+  return acc
+}, {})
+
+const bundleFolder = {
+  development: './build',
+  production: './dist',
+}
+
+// Build folder
+const dir = bundleFolder[parameters.env]
+
+const log = (message) => {
+  // eslint-disable-next-line no-console
+  console.log(`### ${message} ###`)
+}
+
+log(`Building for env: ${parameters.env}`)
+
+// check if directory exists
+if (fs.existsSync(dir)) {
+  rimraf(dir).then(() => {
+    fs.mkdirSync(dir)
+  })
+} else {
+  fs.mkdirSync(dir)
+}
